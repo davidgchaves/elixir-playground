78fa429e5898976c8ae5fefd722dab29e74126b9
David G Chaves <davidgchaves@gmail.com>
2014-10-13 10:02:39 +0200

    Default parameters with pattern matching

--- 06_modules_and_named_functions/default_params.exs
+++ 06_modules_and_named_functions/default_params.exs
@@ -1,5 +1,10 @@
 defmodule DefaultParams do
   def example(p1, p2 \\ 2, p3 \\ 3, p4), do: IO.inspect [p1, p2, p3, p4]
+
+  def with_pattern_matching(p1, p2 \\123)
+  def with_pattern_matching(p1, 123),     do: IO.puts "You used the default"
+  def with_pattern_matching(p1, 99),      do: IO.puts "99 is coming"
+  def with_pattern_matching(p1, p2),      do: IO.puts "You used #{p2}"
 end
 
 # iex(1)> DefaultParams.example("a", "b")
@@ -10,3 +15,13 @@ end
 
 # iex(3)> DefaultParams.example("a", "b", "c", "d")
 # ["a", "b", "c", "d"]
+
+
+# iex(1)> DefaultParams.with_pattern_matching(0, 123)
+# You used the default
+
+# iex(2)> DefaultParams.with_pattern_matching(0, 99)
+# 99 is coming
+
+# iex(3)> DefaultParams.with_pattern_matching(0, 10)
+# You used 10
