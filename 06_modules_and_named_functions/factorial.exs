7fc70c298d56f08b9b4f0affdfe615cbd3770c54
David G Chaves <davidgchaves@gmail.com>
2014-10-13 09:37:15 +0200

    Guarding Factorial.of(n) against a negative n

--- 06_modules_and_named_functions/factorial.exs
+++ 06_modules_and_named_functions/factorial.exs
@@ -1,4 +1,11 @@
 defmodule Factorial do
   def of(0), do: 1
-  def of(n), do: n * of(n-1)
+  def of(n) when n > 0, do: n * of(n-1)
 end
+
+# iex(1)> Factorial.of(-3)
+# ** (FunctionClauseError) no function clause matching in Factorial.of/1
+#     06_modules_and_named_functions/factorial.exs:2: Factorial.of(-3)
+
+# iex(1)> Factorial.of(3)
+# 6
