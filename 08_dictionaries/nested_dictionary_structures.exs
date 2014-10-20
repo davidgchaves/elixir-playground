36400f15363d83cec807efc0afb622e1cc31b9b6
David G Chaves <davidgchaves@gmail.com>
2014-10-20 17:38:52 +0200

    Nested Dictionary Structures 101

---
+++ 08_dictionaries/nested_dictionary_structures.exs
@@ -0,0 +1,15 @@
+defmodule Customer do
+  defstruct name: "", company: ""
+end
+
+defmodule BugReport do
+  defstruct owner: %{}, details: "", severity: 1
+end
+
+# iex(1)> report = %BugReport{ owner: %Customer{ name: "Dave", company: "Pragmatic" }, details: "broken" }
+# %BugReport{details: "broken",
+#            owner: %Customer{company: "Pragmatic", name: "Dave"},
+#            severity: 1}
+
+#iex(2)> report.owner.company
+#"Pragmatic"
