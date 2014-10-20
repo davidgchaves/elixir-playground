73752c81c8adb35d485ab6a65634d3ad995d2453
David G Chaves <davidgchaves@gmail.com>
2014-10-20 18:01:46 +0200

    Supplying keys as symbols in nested accessors

--- 08_dictionaries/nested_dictionary_structures.exs
+++ 08_dictionaries/nested_dictionary_structures.exs
@@ -1,8 +1,10 @@
 defmodule Customer do
+  @derive Access
   defstruct name: "", company: ""
 end
 
 defmodule BugReport do
+  @derive Access
   defstruct owner: %{}, details: "", severity: 1
 end
 
@@ -20,6 +22,20 @@ end
 #            severity: 1}
 
 # iex(4)> update_in(report.owner.name, &("Mr. " <> &1))
+# %BugReport{details: "broken",
+#            owner: %Customer{company: "Pragmatic", name: "Mr. Dave"},
+#            severity: 1}
+
+### Thanks to @derive Access:
+# iex(1)> report = %BugReport{ owner: %Customer{ name: "Dave", company: "Pragmatic" }, details: "broken" }
+# %BugReport{details: "broken",
+#            owner: %Customer{company: "Pragmatic", name: "Dave"},
+#            severity: 1}
+# iex(2)> put_in(report[:owner][:company], "PragProg")
+# %BugReport{details: "broken",
+#            owner: %Customer{company: "PragProg", name: "Dave"},
+#            severity: 1}
+# iex(3)> update_in(report[:owner][:name], &("Mr. " <> &1))
 # %BugReport{details: "broken",
 #            owner: %Customer{company: "Pragmatic", name: "Mr. Dave"},
 #            severity: 1}
